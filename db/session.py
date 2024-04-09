c954acf3e08208544126b4abfd22ba20c88ecfd7
Saiprabhu007 <saiprabhu.dandanayak@zemosolabs.com>
2024-04-09 13:21:46 +0530

    📝 fix : updated session.py file

--- db/session.py
+++ db/session.py
@@ -1,16 +1,21 @@
-
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from core.config import DATABASE_URL
 
 
-database_url=DATABASE_URL
+DEFAULT_DATABASE_URL = 'sqlite:///./test.db'
 
-engine=create_engine(database_url)
 
-Base=declarative_base()
+database_url = DATABASE_URL if DATABASE_URL else DEFAULT_DATABASE_URL
 
+engine = create_engine(database_url)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+Base.metadata.create_all(bind=engine)
 
 sessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
 
